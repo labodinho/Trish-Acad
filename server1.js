5b364be9e2ada8d9ee51f11911fb0d3d80997a10
Oritogun olabode <59852126+labodinho@users.noreply.github.com>
2020-03-07 15:51:35 +0100

    Get started

---
+++ server1.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const http = require("http");
+const path = require("path");
+
+
+
+//bodyparser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//static folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+//send all request to index
+app.set('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+});
+
+
+//set port
+const port = process.env.PORT || "2000";
+app.set("port", port);
+
+const server = http.createServer(app);
+server.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
